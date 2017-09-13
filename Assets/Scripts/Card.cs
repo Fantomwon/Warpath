57be1e31ea6724f8b2ccd3658ecca6cdf6746a97
DESKTOP-QAIMJQJ\Deerok <derekmcdevitt@gmail.com>
2017-09-13 15:33:11 -0700

    -Big improvements to the 'tell' system (see below) -Heroes now 'tell' where they are attempting to target with a certain colored box on the ground (currently gray), and then every square that they are actually attacking will be colored differently (currently red) -Adjusted a ton of nit picky shit with 'tell' box sizes and the grass tiled background position. I think it looks better, but who knows, time may tell!

--- Assets/Scripts/Card.cs
+++ Assets/Scripts/Card.cs
@@ -150,7 +150,7 @@ public class Card : MonoBehaviour {
 		} else if (cardName == "Might") {
 			if (playField.player1Turn) {
 				foreach (Transform hero in playField.player1.transform) {
-					//If one of my heroes is in the square I clicked on then give them the 'Shroud' buff for the appropriate duration
+					//If one of my heroes is in the square I clicked on then give them the 'Might' buff for the appropriate duration
 					if (hero.transform.position.x == playField.roundedPos.x && hero.transform.position.y == playField.roundedPos.y) {
 						Instantiate(spellParticle,hero.transform.localPosition, Quaternion.identity, hero.transform);
 						buffManager.ApplyBuff("might", hero);
@@ -161,7 +161,7 @@ public class Card : MonoBehaviour {
 				}
 			} else if (!playField.player1Turn) {
 				foreach (Transform hero in playField.player2.transform) {
-					//If one of my heroes is in the square I clicked on then give them the 'Shroud' buff for the appropriate duration
+					//If one of my heroes is in the square I clicked on then give them the 'Might' buff for the appropriate duration
 					if (hero.transform.position.x == playField.roundedPos.x && hero.transform.position.y == playField.roundedPos.y) {
 						Instantiate(spellParticle,hero.transform.localPosition, Quaternion.identity, hero.transform);
 						buffManager.ApplyBuff("might", hero);
